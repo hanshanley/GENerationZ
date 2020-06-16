f1b983b22f85d5acd20967fcc5bf89b7247649e8
hanshanley <hhanley@alumni.princeton.edu>
2020-06-16 08:49:21 +0100

    New Models for TCN

--- Utils/proc_chem.py
+++ Utils/proc_chem.py
@@ -31,22 +31,22 @@ def to_smiles(mol):
     return rkc.MolToSmiles(mol, isomericSmiles=False)
 
 def randomize_smiles_string(smile, random_type= 'restricted'):
- 	"""
-    Returns a random SMILES given a SMILES of a molecule.
-    :param smile: A string object
-    :param random_type: The type (unrestricted, restricted) of randomization performed.
-    :return : A random SMILES string of the same molecule or None if the molecule is invalid.
-    """
-    if not smile:
-        return None
+	"""
+	Returns a random SMILES given a SMILES of a molecule.
+	:param smile: A string object
+	:param random_type: The type (unrestricted, restricted) of randomization performed.
+	:return : A random SMILES string of the same molecule or None if the molecule is invalid.
+	"""
+	if not smile:
+		return None
 
-    mol = to_smiles(smile)
+	mol = to_smiles(smile)
 
-    if random_type == "unrestricted":
-        return rkc.MolToSmiles(mol, canonical=False, doRandom=True, isomericSmiles=False)
-    if random_type == "restricted":
-        new_atom_order = list(range(mol.GetNumAtoms()))
-        random.shuffle(new_atom_order)
-        random_mol = rkc.RenumberAtoms(mol, newOrder=new_atom_order)
-        return rkc.MolToSmiles(random_mol, canonical=False, isomericSmiles=False)
-    raise ValueError("Type '{}' is not valid".format(random_type))
+	if random_type == "unrestricted":
+		return rkc.MolToSmiles(mol, canonical=False, doRandom=True, isomericSmiles=False)
+	if random_type == "restricted":
+		new_atom_order = list(range(mol.GetNumAtoms()))
+		random.shuffle(new_atom_order)
+		random_mol = rkc.RenumberAtoms(mol, newOrder=new_atom_order)
+		return rkc.MolToSmiles(random_mol, canonical=False, isomericSmiles=False)
+	raise ValueError("Type '{}' is not valid".format(random_type))
